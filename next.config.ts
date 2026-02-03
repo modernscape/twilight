895d406e12687172a055cd9785b4f79061d663c6
modernscape <modernscape@gmail.com>
2026-02-03 11:19:07 +0900

    テストrepo用のbasePath設定追加

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig = {
   output: "export",
   // GitHub Actions でビルドされる時だけリポジトリ名をパスに含める
   // basePath: isProd ? "/twilight" : "",
+  basePath: process.env.GITHUB_REPOSITORY && !process.env.IS_CUSTOM_DOMAIN ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}` : "",
   images: {
     unoptimized: true,
   },
